daa136cb67a00059dcb5e80ec7b710752a4c16f1
Renan Gomes <contato@renansgomes.com.br>
2021-10-10 19:26:38 -0300

    Seção 13 - StringBuilder

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## Índices
 - [Seção 13 :: Enumerações e Composições](./secao_12_composicao_ex_1)
+- [Seção 13 :: Exercício resolvido 2 (demo StringBuilder)](./secao_13_composicao_ex2_StringBuilder)
+
+## Tópicos Especiais
+- [StringBuilder](./string_builder)
---
+++ string_builder/src/application/Main.java
@@ -0,0 +1,39 @@
+package application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("José\n");
+		sb.append("Por Carlos Drummond de Andrade\n\n");
+		sb.append("E agora, José?\n");
+		sb.append("A festa acabou,\n");
+		sb.append("a luz acabou,\n");
+		sb.append("o povo sumiu,\n");
+		sb.append("a noite esfriou,\n");
+		sb.append("e agora, José?\n");
+		sb.append("e agora, você?\n");
+		sb.append("você que é sem nome,\n");
+		sb.append("que zomba dos outros,\n");
+		sb.append("você que faz versos,\n");
+		sb.append("que ama, protesta?\n");
+		sb.append("e agora, José?\n\n");
+		sb.append("Está sem mulher,\n");
+		sb.append("está sem discurso,\n");
+		sb.append("está sem carinho,\n");
+		sb.append("já não pode beber,\n");
+		sb.append("já não pode fumar,\n");
+		sb.append("cuspir já não pode,\n");
+		sb.append("a noite esfriou,\n");
+		sb.append("o dia não veio,\n");
+		sb.append("o bonde não veio,\n");
+		sb.append("o riso não veio,\n");
+		sb.append("não veio a utopia,\n");
+		sb.append("e tudo acabou\n");
+		sb.append("e tudo fugiu\n");
+		sb.append("e tudo mofou,\n");
+		sb.append("e agora José?\n");
+		System.out.print(sb.toString());
+	}
+
+}
